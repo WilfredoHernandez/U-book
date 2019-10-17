37f740acebdd95ff7df4a1ab8de9bce9599c092e
Omar <fugon55@hotmail.com>
2019-10-17 17:16:53 -0600

    se borraron lineas de codigo que no eran necesarias para el codigo por el momento y se cambio algunas frm del login

--- vistas/login.php
+++ vistas/login.php
@@ -10,26 +10,16 @@ require_once "dependencies.php"
 	<title>Login</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
+	
 	<link rel="icon" type="image/png" href="assets/images/icons/login.ico"/>
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="assets/vendor/bootstrap/js/bootstrap.min.css">
-<!--===============================================================================================-->
+
 	<link rel="stylesheet" type="text/css" href="assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
+
 	<link rel="stylesheet" type="text/css" href="assets/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="assets/vendor/animate/animate.css">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="assets/vendor/css-hamburgers/hamburgers.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="assets/vendor/select2/select2.min.css">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="assets/vendor/daterangepicker/daterangepicker.css">
-<!--===============================================================================================-->
+
 	<link rel="stylesheet" type="text/css" href="assets/css/util.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/login.css">
-<!--===============================================================================================-->
+
 </head>
 <body>
 	
@@ -84,8 +74,8 @@ require_once "dependencies.php"
 
 <script type="text/javascript">
 	$(document).ready(function(){
-		$('#signupS').click(function(){
-		vacios=validarFormVacio('frmSignup');
+		$('#btnLogin').click(function(){
+		vacios=validarFormVacio('frmLogin');
 			if(vacios > 0){
 				alertify.error("All fields are required");
 				return false;
