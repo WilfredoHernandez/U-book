eb5a49905fad820b5ae9f26149e3689b270fc3a5
Omar <fugon55@hotmail.com>
2019-10-19 00:26:01 -0600

    se modifico la ruta de acceso de explorer.php,se agrego el carrusel

--- vistas/explorer.php
+++ vistas/explorer.php
@@ -1,33 +1,79 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <link rel="stylesheet" href="vistas/assets/css/explorer.css">
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" type="text/css" href="assets/css/slick.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/slick-theme.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/slick-design.css">
+  <link rel="stylesheet" href="assets/css/explorer.css">
+  <style type="text/css">
+   
+  </style>
 </head>
 <body>
-
-<ul>
-  <li><a class="shadow"></a></li>
-  <span style="font-size:45px;cursor:pointer;padding:25px"  onclick="openNav()">&#9776;</span>
-<div id="mySidenav" class="sidenav">
-  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-  <a href="#">About</a>
-  <a href="#">Services</a>
-  <a href="#">Clients</a>
-  <a href="#">Contact</a>
-</div>
-</ul>
 <div class="container">
-  
-        <input type="text" placeholder="Look for ISBN...">
+<span style="font-size:45px;cursor:pointer; padding-left: 50px; padding-top: 50px"  onclick="openNav()">&#9776;</span>
+<input type="text" placeholder="Look for ISBN...">
         <div class="search"></div>
-      </div>
+        
+</div>
+  
+              <div id="mySidenav" class="sidenav">
+                <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+                <a href="#">Categories</a>
+                <a></a><a></a><a></a><a></a>
+                <a href="#">Services</a>
+                <a></a><a></a><a></a><a></a>
+                <a href="#">Clients</a>
+                <a></a><a></a><a></a><a></a>
+                <a href="#">Contact</a>
+                <a></a><a></a><a></a><a></a><a></a><a></a><a></a><a></a>
+</div>            
 
+
+<div>
       
+      <section class="regular slider">
+    <div>
+      <img src="../img/Macro.jpg" width="130px" height="350px">
+    </div>
+    <div>
+      <img src="../img/Public Finance.jpg" width="130px" height="350px">
+    </div>
+    <div>
+      <img src="../img/Personal Finance.jpg" width="1px" height="350px">
+    </div>
+    <div>
+      <img src="../img/entrepen.jpg" width="130px" height="350px">
+    </div>
+    <div>
+      <img src="../img/icons/interchange.png" width="130px" height="350px">
+    </div>
+    <div>
+      <img src="../img/icons/interchange.png" width="130px" height="350px">
+    </div>
 
+   </section>
+<div>
+  <script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
+  <script src="assets/css/slick.js" type="text/javascript" charset="utf-8"></script>
+  <script type="text/javascript">
+    $(document).on('ready', function() {
+ 
+      $(".regular").slick({
 
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+      });
+
+    });
+</script>
+</div>
+</body>
+</html>
 
 <script>
 function openNav() {
