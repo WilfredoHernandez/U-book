0edfafb289ce6807743993484b6a44a13205d158
Wilfredo Hernandez <wilfredo.munguiafb@gmail.com>
2019-10-27 14:08:08 -0600

    Se agrego el proceso de cierre de sesion

---
+++ vistas/assets/process/salir.php
@@ -0,0 +1,24 @@
+
+<?php
+//session_start();
+
+//session_destroy();
+
+header("location:../../../index.php")
+
+
+?>
+
+<script>
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
+</script>
+
+
+
+
+
