2603d12339b5e9d997e7b3018f8289dd19d6ff97
Omar <fugon55@hotmail.com>
2019-11-12 23:21:15 -0600

    nuevo css donde se acomoda la imagen del robot para que este en el centro

---
+++ vistas/Error404.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="assets/css/Error404.css">
+    <title>Error 404</title>
+</head>
+<body>
+<h1 align=center>Error 404</h1>
+<p align=center>Page not found in the directory.</p>
+<div class="image">
+<img src="../img/Fixing.jpg" height="250" width="250"  alt="Error">
+</div>
+
+</body>
+</html>
